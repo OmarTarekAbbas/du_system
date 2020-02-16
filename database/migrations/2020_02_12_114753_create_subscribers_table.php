c6f76cef71cb63b5daf6c6564e238ddec09484ec
mohamed mahmoud <m.mahmoud@ivas.com.eg>
2020-02-16 14:59:35 +0200

    fix migrate

--- database/migrations/2020_02_12_114753_create_subscribers_table.php
+++ database/migrations/2020_02_12_114753_create_subscribers_table.php
@@ -16,7 +16,7 @@ class CreateSubscribersTable extends Migration {
 		{
 			$table->increments('id');
 			$table->bigInteger('activation_id')->unsigned()->index('sub_act_fk');
-			$table->date('next_charging_date')->default('0000-00-00');
+			$table->date('next_charging_date')->nullable();
 			$table->date('subscribe_date');
 			$table->boolean('final_status')->default(1)->comment('0=not active , 1=active');
 			$table->boolean('charging_cron')->default(0)->comment('0=  cronfail, 1= run success');
