a2f50debdf4f968927bb683b67bdaf651befe497
emad.mohamed.ivas <emad@ivas.com.eg>
2020-02-24 21:45:06 +0200

    emad,add sechedule run

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
 {
     public function msisdn(Request $request)
     {
-        $result = Activation :: where("msisdn",$request->msisdn)->where("serviceid",$request->serviceid)->where("status_code",0)->orderBy("created_at", "desc")->first(['id','msisdn','serviceid']);
+        $result = Activation :: where("msisdn",$request->msisdn)->where("serviceid",$request->serviceid)->orderBy("created_at", "desc")->first(['id','msisdn','serviceid']);
         if($request->msisdn == $result["msisdn"]){
             $sub    = Subscriber :: where("activation_id",$result->id)->first();
             $unsub  = new Unsubscriber();
