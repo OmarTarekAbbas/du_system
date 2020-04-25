8226f76f91dcc38d0dfa77f3100a9f6ce04edf4b
emad.mohamed.ivas <emad@ivas.com.eg>
2020-04-25 23:25:15 +0200

    emad,add design 2

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -1474,7 +1474,7 @@ class UrlController extends Controller
                 } elseif ($serviceid == "flaterrotanadaily") {
                     $du_welcome_message .= " For Unsubcribe  https://bit.ly/2UB9wfs";
                 }elseif ($serviceid == "liveqarankhatma") {
-                    $du_welcome_message = "Wishing you a very blessed Ramadan.Welcome to Alfasy Quran Live streaming service Live Timing : 05:00 PM – 05:40 PM Thanks for Waiting https://bit.ly/2XX83Dc Daily charges(2/-AED),to unsubscribe send Stop1 to 4971";
+                    $du_welcome_message = "Hi,  Wishing you a very blessed Ramadan.Welcome to Alfasy Quran streaming service  https://bit.ly/2XX83Dc Daily charges(2/-AED),to unsubscribe send Stop to 4971 ";
                     $du_welcome_message .= " ";
                     $du_welcome_message .= "عزيزي مشترك دو كل عام وانتم بخير.مرحبا بكم في البث المباشر  لختمة القرآن مع الشيخ مشاري راشد العفاسي توقيت العرض اليومي الساعة الخامسة مساء يخصم ٢درهم/يومياً";
                     $du_welcome_message .= " ";
