6ef0e30118c8b4e2eea24b26676f14a5470eeb0d
emad.mohamed.ivas <emad@ivas.com.eg>
2020-05-05 16:39:46 +0200

    emad,edit

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -1790,7 +1790,7 @@ class UrlController extends Controller
 
         if ($request->message ==  "1" ||  $request->message == "A"   ||  $request->message == "Alafasy"
         ||  $request->message == "alafasy" ||  $request->message == "AFASY"  ||  $request->message == "Afasy"    ||  $request->message == "العفاسي" ||  $request->message == "عفاسي"
-     ||    $request->message == "Afasi"  ||    $request->message == "afasi"   ) {//sub to quran live
+     ||    $request->message == "Afasi"  ||    $request->message == "afasi" ||    $request->message == ""  ||    $request->message == " "  ) {//sub to quran live
             require('uuid/UUID.php');
             $trxid = \UUID::v4();
             $URL = url('api/activation');
@@ -1798,7 +1798,7 @@ class UrlController extends Controller
             $result = $this->get_content_post($URL, $param);
             $this->log('DU MO Quran Live Subscription Notification', $request->fullUrl(), (array)$result);
             return $result;
-        } else if ($request->message == 'Stop1' ||  $request->message == 'stop1'  ||  $request->message == 'stop'  ||  $request->message == 'Stop' ||  $request->message == " " ) {// unsub from quran live
+        } else if ($request->message == 'Stop1' ||  $request->message == 'stop1'  ||  $request->message == 'stop'  ||  $request->message == 'Stop' ) {// unsub from quran live
             $result = $result->where('serviceid', 'liveqarankhatma');
             $result = $result->latest("created_at")->first(['id', 'msisdn', 'serviceid']);
             if ($result) {
