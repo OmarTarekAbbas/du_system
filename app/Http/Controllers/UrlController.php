ecf9104ca48a5788f3054ae5b7af12fcfb5dfb67
emad.mohamed.ivas <emad@ivas.com.eg>
2020-05-10 17:41:03 +0200

    emad,edit

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -434,7 +434,15 @@ class UrlController extends Controller
             $data['msisdns'] = $subscribers;
             if ($subscribers->count() > 0) {
                 $message = \App\Message::where('service_id', $service->id)->where('date', $today)->whereNull('IsysResponse')->first();
-                echo  $message->id ; die;
+                if( $message ){
+                    echo "send" ;
+
+                }else{
+                    echo "no" ;
+                }
+
+                die;
+
                 if ($message) {
                     $data['message'] = $message->MTBody . ' ' . $message->ShortnedURL;
                     array_push($all, $data);
